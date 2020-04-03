7696436d4d37127aa237f9f6514241ce7803e0ae
Sergio <seranmoreno500@gmail.com>
2020-04-03 17:10:52 +0200

    images Size
    asd

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,16 +3,32 @@
 // SCROLLING EFFECT FOR BOXES
 $(document).ready(function(e){
     
-    var lastScrollTop = 0;
-    $(window).scroll(function(e){
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop){
-            // downscroll code
-        } else {
-            // upscroll code
-        }
-        lastScrollTop = st;
-    });
-    console.log(lastScrollTop);
+    let lastScrollTop = 0;
+    let boxes = $("#gallery");
+    let initialTop = parseInt(boxes.css("top").replace("px", ""));
+    let speed = 7;
 
+    $(window).scroll(function(e){
+        let st = $(this).scrollTop();
+
+        if (st > lastScrollTop){
+            move(speed);
+        } else {
+            move(-speed);
+        }
+        if(st == 0){
+            boxes.animate({
+                top: initialTop, 
+            }, 50);
+        }
+
+        lastScrollTop = st;
+        console.log(boxes.css("top"));
+    });
+    
+    function move(speed){
+        boxes.animate({
+            top: "-="+speed, 
+        }, 10);
+    };
 });
