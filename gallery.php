2b75f980055f93be41261400dee8c58819796af2
Sergio <seranmoreno500@gmail.com>
2020-04-07 11:24:20 +0200

    Change in img from galery to index
    asd

--- gallery.php
+++ gallery.php
@@ -1,5 +1,29 @@
 <?php
 
-echo("Php tira de puta madre en el servidor")
+    
+    
+    $directory = getDirContents('./resources/img/gallery');
 
+    for ($i=0; $i < sizeof($directory); $i++) { 
+        var_dump($directory[$i]);
+        echo("<br>");
+    }
+
+
+    function getDirContents($dir, &$results = array()) {
+        $files = scandir($dir);
+    
+        foreach ($files as $key => $value) {
+            $path = ($dir . DIRECTORY_SEPARATOR . $value);
+
+            if (!is_dir($path)) {
+                $results[] = $path;
+            } else if ($value != "." && $value != "..") {
+                getDirContents($path, $results);
+                $results[] = $path;
+            }
+        }
+    
+        return $results;
+    }
 ?>
